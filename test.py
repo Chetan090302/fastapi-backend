7f4ad6a116032874817b63b284eb51e0e93e7c79
JETTY CHETAN <jettychetan524@gmail.com>
2026-05-16 16:11:32 +0530

    123

--- test.py
+++ test.py
@@ -37,3 +37,12 @@ async def fake_llm_call(input_data=None):
         chunk = f"data: Message {i}\n\n"
         yield chunk.encode("utf-8")
         await asyncio.sleep(5)
+
+async def main_method():
+    input_data=["capital of france","capital of germany","capital of italy"]
+    response=asyncio.map(call_model,input_data)
+    return response
+
+
+#hello world
+
